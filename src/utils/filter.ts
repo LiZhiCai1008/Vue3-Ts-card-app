cacdb6c137e454b5a98ab4358cb72398dce5d9ca
lizhicai <zhicai.li@lerke.cn>
2021-06-11 18:39:22 +0800

    fix：buy-limit

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,28 +1,21 @@
-// /**
-//  * Created by lizhicai on 5/31/2021.
-//  */
+/**
+ * Created by lizhicai on 5/31/2021.
+ */
 // import fl from "./float";
 
-// export const amountFmt = (value: any) => {
-//   return fl.float.divide(value, 100, "divide").toFixed(2);
-// }
-// export default {
-//   install: Vue => {
-//     Vue.filter("amountFmt", amountFmt);
-
-//     Vue.filter("statusFmt", function (value) {
-//       let statusText = {
-//         1: "正常",
-//         2: "禁用",
-//         3: "挂失",
-//         4: "补卡中",
-//         99: "注销",
-//       }
-//       return statusText[value];
-//     });
-
-//     Vue.filter("rateFmt", function (value: any) {
-//       return fl.float.multiply(value, 100, "multiply");
-//     });
-//   }
-// };
+export const amountFmt = (value: number): string => {
+  return (value / 100).toFixed(2);
+}
+export const statusFmt = (value: number): string => {
+  interface TypeStatus {
+    [key: number]: string
+  }
+  const statusText: TypeStatus = {
+    1: "正常",
+    2: "禁用",
+    3: "挂失",
+    4: "补卡中",
+    99: "注销",
+  }
+  return statusText[value];
+}
--- src/views/card/buy-limit.vue
+++ src/views/card/buy-limit.vue
@@ -1,9 +1,9 @@
 <template>
   <Page isHeader>
     <div class="limit-form">
-      <van-form ref="limitForm">
+      <van-form @submit="handleClickSave" @failed="onFailed">
         <van-field
-          v-model="limitForm.singleDayLimit"
+          v-model="singleDayLimit"
           :rules="[
             { validator: validatorPrice, message: '金额输入有误' }
           ]"
@@ -16,7 +16,7 @@
           maxlength="7"
         />
         <van-field
-          v-model="limitForm.singleOrderLimit"
+          v-model="singleOrderLimit"
           :rules="[
             { validator: validatorPrice, message: '金额输入有误' }
           ]"
@@ -29,7 +29,7 @@
           maxlength="7"
         />
         <van-field
-          v-model="limitForm.orderNumLimit"
+          v-model="orderNumLimit"
           :rules="[
             { validator: validatorOrderNum, message: '请输入0-99内正整数' }
           ]"
@@ -41,89 +41,95 @@
           autocomplete="off"
           maxlength="3"
         />
+        <div class="content defalut-bg">
+          <p class="lk-p-t-16 font-3 lk-font-14">数值为0或者为空，无限制</p>
+          <van-button
+            class="lk-m-t-40 font-1"
+            color="#F8B500"
+            block
+            round
+            @click="handleClickSave"
+          >保存</van-button>
+        </div>
       </van-form>
-    </div>
-    <div class="content">
-      <p class="lk-m-t-16 font-3 lk-font-14">数值为0或者为空，无限制</p>
-      <van-button
-        class="lk-m-t-40 font-1"
-        color="#F8B500"
-        block
-        round
-        @click="handleClickSave"
-      >保存</van-button>
     </div>
   </Page>
 </template>
 
-<script>
-import { updateLimitApi } from '@/api/card'
-import { mapState } from 'vuex'
+<script lang="ts">
+import { CardService } from '@/api/card'
 import { amountFmt } from '@/utils/filter'
-export default {
+import { Toast } from 'vant'
+import { useStore } from 'vuex'
+import { defineComponent, computed, reactive, toRefs, onBeforeMount } from "vue";
+import { useRouter } from 'vue-router'
+export default defineComponent({
   name: "BuyLimit",
-  data() {
-    return {
-      updateLimitApi,
-      limitForm: {
-        singleDayLimit: "",
-        singleOrderLimit: "",
-        orderNumLimit: ""
-      }
-    }
-  },
-  beforeRouteEnter (to, from, next) {
-    // ...
-    next(vm => {
-      vm.limitForm.singleDayLimit = vm.cardInfo.singleDayLimit ? amountFmt(vm.cardInfo.singleDayLimit) : ""
-      vm.limitForm.singleOrderLimit = vm.cardInfo.singleOrderLimit ? amountFmt(vm.cardInfo.singleOrderLimit) : ""
-      vm.limitForm.orderNumLimit = vm.cardInfo.orderNumLimit || ""
+  setup() {
+    const router = useRouter()
+    // 创建仓库
+    const store = useStore()
+    const limitForm = reactive({
+      singleDayLimit: "",
+      singleOrderLimit: "",
+      orderNumLimit: ""
     })
-  },
-  computed: {
-    ...mapState("card", ["cardInfo"])
-  },
-  methods: {
-    validatorPrice(value) {
+    const cardInfo = computed(() => store.state.card.cardInfo)
+    onBeforeMount(() => {
+      limitForm.singleDayLimit = cardInfo.value.singleDayLimit ? amountFmt(Number(cardInfo.value.singleDayLimit)) : ""
+      limitForm.singleOrderLimit = cardInfo.value.singleOrderLimit ? amountFmt(Number(cardInfo.value.singleOrderLimit)) : ""
+      limitForm.orderNumLimit = cardInfo.value.orderNumLimit || ""
+    })
+
+    const validatorPrice = (value: string): boolean => {
       if (!value) return true
       let priceNum = Number(value)
       if (priceNum < 0 || priceNum >= 1000) {
         return false
       } else {
-        return /^\d+(.\d{1,2})?$/.test(priceNum)
+        return /^\d+(.\d{1,2})?$/.test(priceNum.toString())
       }
-    },
-    validatorOrderNum(value) {
+    }
+
+    const validatorOrderNum = (value: string): boolean => {
       if (!value) return true
       let priceNum = Number(value)
       if (priceNum < 0 || priceNum >= 100) {
         return false
       } else return true
-    },
-    async handleClickSave() {
-      console.log('save', this.limitForm)
-      let that = this
-      this.$refs.limitForm.validate().then(async() => {
-        try {
-          let card = JSON.parse(localStorage.getItem("CARD_INFO"))
-          const { code } = await that.updateLimitApi({
-            cardNo: that.cardInfo.cardNo || card.cardNo,
-            serialNo: that.cardInfo.serialNo || card.serialNo,
-            ...that.limitForm
-          })
-          if (code === 200) {
-            that.$toast("提交成功！")
-            that.$router.back()
-          }
-        } catch (error) {
-          console.log(error)
+    }
+
+    const handleClickSave = async (): Promise<any> =>{
+      console.log('save', limitForm)
+      try {
+        let card = JSON.parse(localStorage.getItem("CARD_INFO") || "")
+        const { data } = await CardService.updateLimitApi({
+          cardNo: cardInfo.value.cardNo || card.cardNo,
+          serialNo: cardInfo.value.serialNo || card.serialNo,
+          ...limitForm
+        })
+        if (data.code === 200) {
+          Toast("提交成功！")
+          router.back()
         }
-      }).catch(() => {
-          that.$toast("信息有误，请检查")
-      })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const onFailed = (errorInfo: any) => {
+      console.log('failed', errorInfo);
+      Toast("信息有误，请检查")
+    }
+    return {
+      ...toRefs(limitForm),
+      validatorPrice,
+      validatorOrderNum,
+      handleClickSave,
+      onFailed
     }
   }
-}
+})
 </script>
 
 <style lang="scss" scoped>
